d81aba99bb5341749c87e11ba21d9506f25f168b
Bguillot <77786971+BGuiIlot@users.noreply.github.com>
2022-04-05 11:45:43 +0200

    Add files via upload

--- routes.js
+++ routes.js
@@ -8,12 +8,25 @@ router.get("/intervention", async (req, res) => {
 	res.send(interventions)
 })
 
-router.patch("/intervention/:code", async (req, res) => {
+router.patch("/intervention/entree/:code", async (req, res) => {
 	try {
-        const interventions = await intervention.findOneAndUpdate(req.params.code,{new : true, $set: {heureDebutReel: new Date(), heureFinReel: new Date()}});
+        const interventions = await intervention.findOneAndUpdate(req.params.code,{new : true, $set: {heureDebutReel: new Date()}});
         Object.assign(interventions, req.body)
         interventions.save()
-        res.send({heureDebutReel: new Date(), heureFinReel: new Date()})
+        res.send({heureDebutReel: new Date()})
+	}
+    catch {
+		res.status(404)
+		res.send({ error: "Update can't works" })
+	}
+})
+
+router.patch("/intervention/sortie/:code", async (req, res) => {
+	try {
+        const interventions = await intervention.findOneAndUpdate(req.params.code,{new : true, $set: {heureFinReel: new Date()}});
+        Object.assign(interventions, req.body)
+        interventions.save()
+        res.send({heureFinReel: new Date()})
 	}
     catch {
 		res.status(404)
